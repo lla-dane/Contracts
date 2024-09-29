caf3d3dd57f74d883533506ea7947527a50c2c4b
lla-dane <abhinavagarwalla6@gmail.com>
2024-09-29 21:25:51 +0530

    Custom ERC20 contracts

---
+++ applications/contracts/ERC20.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: UNLICENSE
+pragma solidity ^0.8.26;
+
+import "./IERC20.sol";
+
+contract ERC20 is IERC20 {
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+    }
+
+    function transfer(address recipient, uint amount) external returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address spender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool) {
+        allowance[spender][msg.sender] -= amount;
+        balanceOf[spender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(spender, recipient, amount);
+        return true;
+    }
+
+    function _mint(address to, uint amount) internal {
+        balanceOf[to] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), to, amount);
+    }
+
+    function _burn(address from, uint256 amount) internal {
+        balanceOf[from] -= amount;
+        totalSupply -= amount;
+        emit Transfer(from, address(0), amount);
+    }
+
+    function mint(address to, uint amount) external {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint amount) external {
+        _burn(from, amount);
+    }
+}
---
+++ applications/contracts/Swap.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import "./IERC20.sol";
+
+contract TokenSwap {
+    IERC20 public token1;
+    address public owner1;
+    uint public amount1;
+    IERC20 public token2;
+    address public owner2;
+    uint256 public amount2;
+
+    constructor(
+        address _token1,
+        address _owner1,
+        uint256 _amount1,
+        address _token2,
+        address _owner2,
+        uint256 _amount2
+    ) {
+        token1 = IERC20(_token1);
+        owner1 = _owner1;
+        amount1 = _amount1;
+        token2 = IERC20(_token2);
+        owner2 = _owner2;
+        amount2 = _amount2;
+    }
+
+    function swap() public {
+        require(msg.sender == owner1 || msg.sender == owner2, "NOT AUTHORIZED");
+        require(
+            token1.allowance(owner1, address(this)) >= amount1,
+            "Token1 allowance low"
+        );
+        require(
+            token2.allowance(owner2, address(this)) >= amount2,
+            "Token2 allowance low"
+        );
+
+        _safeTransferFrom(token1, owner1, owner2, amount1);
+        _safeTransferFrom(token2, owner2, owner1, amount2);
+    }
+
+    function _safeTransferFrom(
+        IERC20 token,
+        address sender,
+        address recipient,
+        uint amount
+    ) private {
+        bool sent = token.transferFrom(sender, recipient, amount);
+        require(sent, "TOKEN TRANSFER FAILED");
+    }
+}
